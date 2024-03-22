c907dae4823e1bf2ff8aef9c2b2bc010545ac050
aidanaden <aidan@u.nus.edu>
2024-03-23 07:45:34 +0800

    test; pages

--- dev/app.config.ts
+++ dev/app.config.ts
@@ -4,11 +4,11 @@ import uno from "unocss/vite";
 export default defineConfig({
   ssr: true,
   server: {
-    preset: process.env.DEVELOPMENT ? "node-server" : "cloudflare-pages-static",
+    preset: process.env.DEVELOPMENT ? "node-server" : "cloudflare-pages",
     // We will need to enable CF Pages node compatiblity
     // https://developers.cloudflare.com/workers/runtime-apis/nodejs/asynclocalstorage/
     rollupConfig: {
-      external: ["__STATIC_CONTENT_MANIFEST", "node:async_hooks"],
+      external: ["node:async_hooks"],
     },
   },
   vite: {
